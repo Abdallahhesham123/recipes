b5eaa20c1f6cb6cff3bfcd9a216aaa6cc04da024
abdallahhesham123 <abdallahhesham2@gmail.com>
2022-11-19 10:04:36 +0200

    version updated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,29 +1,63 @@
-import React from 'react'
+import React ,{useState}from 'react'
 import styled from "./navbar.module.css"
-import {Link} from "react-router-dom";
+import { Link,NavLink} from "react-router-dom";
 export default function Navbar() {
+
+  const [NAVBAR, setNAVBAR] = useState(false)
+
+  const ChangeBackGround=()=>{
+
+    // console.log('====================================');
+    // console.log(window.scrollY);
+    // console.log('====================================');
+    if(window.scrollY >= 120){
+      setNAVBAR(true)
+    }else{
+      setNAVBAR(false)
+    }
+  }
+  window.addEventListener("scroll",ChangeBackGround)
+
+  const navBarStyles=({ isActive })=>{
+    return{
+
+
+      color: isActive ? 'black' : '#1ABC9C',
+      background: isActive ? '#1ABC9C' : 'transparent',
+      borderRadius:isActive ? '15px' : '',
+      border:isActive ? '2px solid #fff' : ''
+    }
+  }
   return (
     <div>
-    <nav className={`${styled.bg_color} navbar navbar-expand-lg navbar-dark w-100 fixed-top`}>
+    <nav className={NAVBAR ? `${styled.active} navbar navbar-expand-lg  w-100 fixed-top `: `${styled.navBar} navbar navbar-expand-lg  w-100 fixed-top `}>
     <div className="container">
-      <a className="navbar-brand" href="/#">Abdallah-Design</a>
+      <Link className="navbar-brand" to="/">Abdallah-Design</Link>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-          <li className="nav-item">
-            <Link className="nav-link active" aria-current="page" to="">Home</Link>
+          <li className="nav-item mx-2">
+            <NavLink className="nav-link active" aria-current="page"
+            style={navBarStyles} 
+            to="">HOME</NavLink>
           </li>
 
-          <li className="nav-item">
-          <Link className="nav-link" to="gallary">Gallary</Link>
+          <li className="nav-item mx-2">
+          <NavLink className="nav-link"
+          style={navBarStyles} 
+          to="gallary">GALLARY</NavLink>
         </li>
-        <li className="nav-item">
-        <Link className="nav-link" to="parent">Parent</Link>
+        <li className="nav-item mx-2">
+        <NavLink className="nav-link" 
+        style={navBarStyles} 
+        to="parent">PARENT</NavLink>
       </li>
-      <li className="nav-item">
-      <Link className="nav-link" to="recipes">Recipes</Link>
+      <li className="nav-item mx-2">
+      <NavLink className="nav-link" 
+      style={navBarStyles} 
+      to="recipes">RECIPES</NavLink>
     </li>
    
         </ul>
